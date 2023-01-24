500b87d58b296c067f65fc7219c706028533566f
Milad Zarour <Miladzarour@hotmail.com>
2023-01-24 18:24:01 +0100

    added extra functionality for HiLo Card

--- HiLo-Card/Assets/Scripts/DealCard.cs
+++ HiLo-Card/Assets/Scripts/DealCard.cs
@@ -6,9 +6,15 @@ public class DealCard : MonoBehaviour
 {
     public GameObject[] dealtCard;
     public int cardGenerate;
+    public GameObject hiButton;
+    public GameObject loButton;
+    public GameObject dealButton;
     public void DealMyNewCard()
     {
         cardGenerate = Random.Range(2, 15);
         dealtCard[cardGenerate].SetActive(true);
+        hiButton.SetActive(true);
+        loButton.SetActive(true);
+        dealButton.SetActive(false);
     }
 }

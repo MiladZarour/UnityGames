8d12da281851490e0c967dbe14800311f7ea1464
Milad Zarour <Miladzarour@hotmail.com>
2023-01-24 19:20:21 +0100

    added dealtright and turnleft cards

--- HiLo-Card/Assets/Scripts/CardControl.cs
+++ HiLo-Card/Assets/Scripts/CardControl.cs
@@ -13,6 +13,8 @@ public class CardControl : MonoBehaviour
     public GameObject hiButton;
     public GameObject loButton;
     public GameObject dealButton;
+    public GameObject[] dealingLeftCards;
+    public GameObject[] turningRightCards;
 
     // equal number = winner
     void Update()
@@ -39,10 +41,73 @@ public class CardControl : MonoBehaviour
         if(newCardNumber >= dealtCardNumber)
         {
             correctText.SetActive(true);
+            yield return new WaitForSeconds(3);
+            dealingLeftCards[2].SetActive(false);
+            dealingLeftCards[3].SetActive(false);
+            dealingLeftCards[4].SetActive(false);
+            dealingLeftCards[5].SetActive(false);
+            dealingLeftCards[6].SetActive(false);
+            dealingLeftCards[7].SetActive(false);
+            dealingLeftCards[8].SetActive(false);
+            dealingLeftCards[9].SetActive(false);
+            dealingLeftCards[10].SetActive(false);
+            dealingLeftCards[11].SetActive(false);
+            dealingLeftCards[12].SetActive(false);
+            dealingLeftCards[13].SetActive(false);
+            dealingLeftCards[14].SetActive(false);
+            turningRightCards[2].SetActive(false);
+            turningRightCards[3].SetActive(false);
+            turningRightCards[4].SetActive(false);
+            turningRightCards[5].SetActive(false);
+            turningRightCards[6].SetActive(false);
+            turningRightCards[7].SetActive(false);
+            turningRightCards[8].SetActive(false);
+            turningRightCards[9].SetActive(false);
+            turningRightCards[10].SetActive(false);
+            turningRightCards[11].SetActive(false);
+            turningRightCards[12].SetActive(false);
+            turningRightCards[13].SetActive(false);
+            turningRightCards[14].SetActive(false);
+            turningRightCards[newCardNumber].SetActive(true);
+            yield return new WaitForSeconds(1);
+            hiButton.SetActive(true);
+            loButton.SetActive(true);
+            dealtCardNumber = newCardNumber;
+            correctText.SetActive(false);
         }
         else
         {
             incorrectText.SetActive(true);
+            yield return new WaitForSeconds(3);
+            dealingLeftCards[2].SetActive(false);
+            dealingLeftCards[3].SetActive(false);
+            dealingLeftCards[4].SetActive(false);
+            dealingLeftCards[5].SetActive(false);
+            dealingLeftCards[6].SetActive(false);
+            dealingLeftCards[7].SetActive(false);
+            dealingLeftCards[8].SetActive(false);
+            dealingLeftCards[9].SetActive(false);
+            dealingLeftCards[10].SetActive(false);
+            dealingLeftCards[11].SetActive(false);
+            dealingLeftCards[12].SetActive(false);
+            dealingLeftCards[13].SetActive(false);
+            dealingLeftCards[14].SetActive(false);
+            turningRightCards[2].SetActive(false);
+            turningRightCards[3].SetActive(false);
+            turningRightCards[4].SetActive(false);
+            turningRightCards[5].SetActive(false);
+            turningRightCards[6].SetActive(false);
+            turningRightCards[7].SetActive(false);
+            turningRightCards[8].SetActive(false);
+            turningRightCards[9].SetActive(false);
+            turningRightCards[10].SetActive(false);
+            turningRightCards[11].SetActive(false);
+            turningRightCards[12].SetActive(false);
+            turningRightCards[13].SetActive(false);
+            turningRightCards[14].SetActive(false);
+            yield return new WaitForSeconds(1);
+            dealButton.SetActive(true);
+            incorrectText.SetActive(false);
         }
     }
     IEnumerator GuessingLower()
@@ -51,10 +116,73 @@ public class CardControl : MonoBehaviour
         if (newCardNumber <= dealtCardNumber)
         {
             correctText.SetActive(true);
+            yield return new WaitForSeconds(3);
+            dealingLeftCards[2].SetActive(false);
+            dealingLeftCards[3].SetActive(false);
+            dealingLeftCards[4].SetActive(false);
+            dealingLeftCards[5].SetActive(false);
+            dealingLeftCards[6].SetActive(false);
+            dealingLeftCards[7].SetActive(false);
+            dealingLeftCards[8].SetActive(false);
+            dealingLeftCards[9].SetActive(false);
+            dealingLeftCards[10].SetActive(false);
+            dealingLeftCards[11].SetActive(false);
+            dealingLeftCards[12].SetActive(false);
+            dealingLeftCards[13].SetActive(false);
+            dealingLeftCards[14].SetActive(false);
+            turningRightCards[2].SetActive(false);
+            turningRightCards[3].SetActive(false);
+            turningRightCards[4].SetActive(false);
+            turningRightCards[5].SetActive(false);
+            turningRightCards[6].SetActive(false);
+            turningRightCards[7].SetActive(false);
+            turningRightCards[8].SetActive(false);
+            turningRightCards[9].SetActive(false);
+            turningRightCards[10].SetActive(false);
+            turningRightCards[11].SetActive(false);
+            turningRightCards[12].SetActive(false);
+            turningRightCards[13].SetActive(false);
+            turningRightCards[14].SetActive(false);
+            turningRightCards[newCardNumber].SetActive(true);
+            yield return new WaitForSeconds(1);
+            hiButton.SetActive(true);
+            loButton.SetActive(true);
+            dealtCardNumber = newCardNumber;
+            correctText.SetActive(false);
         }
         else
         {
             incorrectText.SetActive(true);
+            yield return new WaitForSeconds(3);
+            dealingLeftCards[2].SetActive(false);
+            dealingLeftCards[3].SetActive(false);
+            dealingLeftCards[4].SetActive(false);
+            dealingLeftCards[5].SetActive(false);
+            dealingLeftCards[6].SetActive(false);
+            dealingLeftCards[7].SetActive(false);
+            dealingLeftCards[8].SetActive(false);
+            dealingLeftCards[9].SetActive(false);
+            dealingLeftCards[10].SetActive(false);
+            dealingLeftCards[11].SetActive(false);
+            dealingLeftCards[12].SetActive(false);
+            dealingLeftCards[13].SetActive(false);
+            dealingLeftCards[14].SetActive(false);
+            turningRightCards[2].SetActive(false);
+            turningRightCards[3].SetActive(false);
+            turningRightCards[4].SetActive(false);
+            turningRightCards[5].SetActive(false);
+            turningRightCards[6].SetActive(false);
+            turningRightCards[7].SetActive(false);
+            turningRightCards[8].SetActive(false);
+            turningRightCards[9].SetActive(false);
+            turningRightCards[10].SetActive(false);
+            turningRightCards[11].SetActive(false);
+            turningRightCards[12].SetActive(false);
+            turningRightCards[13].SetActive(false);
+            turningRightCards[14].SetActive(false);
+            yield return new WaitForSeconds(1);
+            dealButton.SetActive(true);
+            incorrectText.SetActive(false);
         }
     }
 }

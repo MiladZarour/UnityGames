c15b7e45fcdb63453005f11f66ae935a3aab7298
Milad Zarour <Miladzarour@hotmail.com>
2023-01-24 18:00:44 +0100

    added GuessCardDeck and TurnCardDeck with the Cards

---
+++ HiLo-Card/Assets/Scripts/DealCard.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DealCard : MonoBehaviour
+{
+    public GameObject dealtCard;
+    public void DealMyNewCard()
+    {
+        dealtCard.SetActive(true);
+    }
+}

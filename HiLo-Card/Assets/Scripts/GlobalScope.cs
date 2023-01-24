d2a0223fb1261a62bf082d08896a1f4cf9ca8587
Milad Zarour <Miladzarour@hotmail.com>
2023-01-24 19:54:02 +0100

    added Best and STRAK score

--- HiLo-Card/Assets/Scripts/GlobalScope.cs
+++ HiLo-Card/Assets/Scripts/GlobalScope.cs
@@ -8,9 +8,22 @@ public class GlobalScope : MonoBehaviour
     
     public static int currentScore;
     public GameObject scoreDisplay;
+    public int highScore;
+    public GameObject bestDisplay;
+
+    void Start()
+    {
+        highScore = PlayerPrefs.GetInt("BestScoreHiLo");
+        bestDisplay.GetComponent<Text>().text = "BEST: " + highScore;
+    }
 
     void Update()
     {
         scoreDisplay.GetComponent<Text>().text = "STREAK: " + currentScore;
+        if (currentScore > highScore)
+        {
+            bestDisplay.GetComponent<Text>().text = "BEST: " + currentScore;
+            PlayerPrefs.SetInt("BestScoreHiLo", currentScore);
+        }
     }
 }

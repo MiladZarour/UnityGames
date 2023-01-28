bee7c320de65d606d3fb85166d8449a43ff96739
Milad Zarour <Miladzarour@hotmail.com>
2023-01-28 16:03:09 +0100

    added another condition to not move more longer than the edges

--- Endless Runner/Assets/Scripts/Player/PlayerMove.cs
+++ Endless Runner/Assets/Scripts/Player/PlayerMove.cs
@@ -12,11 +12,20 @@ public class PlayerMove : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
         if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
+            if(this.gameObject.transform.position.x > LevelBoundary.leftSide)
+            {
+
+                transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
+            }
+            
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1);
+            if (this.gameObject.transform.position.x < LevelBoundary.rightSide)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1);
+            }
+            
         }
     }
 }

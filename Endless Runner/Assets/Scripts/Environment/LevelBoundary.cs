d02a6e3991c76a52a506ca478a3d0ad4ef19dc7e
Milad Zarour <Miladzarour@hotmail.com>
2023-01-28 11:01:30 +0100

    added Environment.cs Code + adding 4 variables LeftSide RightSide internalLeft internalRight , static is to allow for using the variable in all the others scripts

---
+++ Endless Runner/Assets/Scripts/Environment/LevelBoundary.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelBoundry : MonoBehaviour
+{
+    public static float leftSide;
+    public static float rightSide;
+    public float internalLeft;
+    public float internalRight;
+
+    void Update()
+    {
+        
+    }
+}

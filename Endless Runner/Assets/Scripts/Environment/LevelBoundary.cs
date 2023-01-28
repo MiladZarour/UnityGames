73325118adfaff1a59b7782f795bd2e60603f57f
Milad Zarour <Miladzarour@hotmail.com>
2023-01-28 14:00:59 +0100

    added left and Right with A and D

--- Endless Runner/Assets/Scripts/Environment/LevelBoundary.cs
+++ Endless Runner/Assets/Scripts/Environment/LevelBoundary.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelBoundry : MonoBehaviour
+public class LevelBoundary : MonoBehaviour
 {
-    public static float leftSide;
-    public static float rightSide;
+    public static float leftSide = -3.5f;
+    public static float rightSide = 3.5f;
     public float internalLeft;
     public float internalRight;
 
     void Update()
     {
-        
+        internalLeft = leftSide;
+        internalRight = rightSide;
     }
 }

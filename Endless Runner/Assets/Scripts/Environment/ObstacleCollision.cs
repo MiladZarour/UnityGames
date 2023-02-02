a4108a4ef91bc09aa46aa75017d377f6de619cac
Milad Zarour <Miladzarour@hotmail.com>
2023-02-02 20:56:09 +0100

    added Rotated Camera when the player hit something

--- Endless Runner/Assets/Scripts/Environment/ObstacleCollision.cs
+++ Endless Runner/Assets/Scripts/Environment/ObstacleCollision.cs
@@ -7,11 +7,15 @@ public class ObstacleCollision : MonoBehaviour
 
     public GameObject thePlayer;
     public GameObject charModel;
+    public AudioSource crashThud;
+    public GameObject mainCam;
 
     void OnTriggerEnter(Collider other)
     {
         this.gameObject.GetComponent<BoxCollider>().enabled = false;
         thePlayer.GetComponent<PlayerMove>().enabled = false;
         charModel.GetComponent<Animator>().Play("Stumble Backwards");
+        crashThud.Play();
+        mainCam.GetComponent<Animator>().enabled = true;
     }
 }

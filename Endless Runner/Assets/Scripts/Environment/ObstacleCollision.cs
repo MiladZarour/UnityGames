af98b0a0edebf1bf8c865a7b2a303622990ed02b
Milad Zarour <Miladzarour@hotmail.com>
2023-02-02 20:37:21 +0100

    added Stumble Backwards

---
+++ Endless Runner/Assets/Scripts/Environment/ObstacleCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleCollision : MonoBehaviour
+{
+
+    public GameObject thePlayer;
+    public GameObject charModel;
+
+    void OnTriggerEnter(Collider other)
+    {
+        this.gameObject.GetComponent<BoxCollider>().enabled = false;
+        thePlayer.GetComponent<PlayerMove>().enabled = false;
+        charModel.GetComponent<Animator>().Play("Stumble Backwards");
+    }
+}

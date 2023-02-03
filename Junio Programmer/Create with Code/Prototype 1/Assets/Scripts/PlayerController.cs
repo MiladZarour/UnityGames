8b945a5ba794f602d76d27501708afe2a842bbd0
Milad Zarour <Miladzarour@hotmail.com>
2023-02-03 21:03:07 +0100

    adding Input.GetAxis(Horizontal)

--- Junio Programmer/Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Junio Programmer/Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,8 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed = 5.0f;
-    public float turnSpeed;
+    public float turnSpeed = 4;
+    public float horizontalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
         transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
     }

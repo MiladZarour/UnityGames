82bd8e2b04f0a884b4afd9f23a3228ba0a6f0a1b
Milad Zarour <Miladzarour@hotmail.com>
2023-02-03 21:14:43 +0100

    Clean up my code

--- Junio Programmer/Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Junio Programmer/Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed = 5.0f;
-    public float turnSpeed = 4;
-    public float horizontalInput;
-    public float forwardInput;
+    private float speed = 5.0f;
+    private float turnSpeed = 45.0f ;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +22,9 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
+        // Moves the car forward based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }

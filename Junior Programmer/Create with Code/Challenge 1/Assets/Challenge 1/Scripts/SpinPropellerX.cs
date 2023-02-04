1ca5d7ec08ee87ed4ba10a9a82dafcfc91fef449
Milad Zarour <Miladzarour@hotmail.com>
2023-02-04 15:48:16 +0100

    changed the name of the file

--- Junior Programmer/Create with Code/Challenge 1/Assets/Challenge 1/Scripts/SpinPropellerX.cs
+++ Junior Programmer/Create with Code/Challenge 1/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Propeller : MonoBehaviour
+public class SpinPropellerX : MonoBehaviour
 {
 
     public float rotationSpeed;

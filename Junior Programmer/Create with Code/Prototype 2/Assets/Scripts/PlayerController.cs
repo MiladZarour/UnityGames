56965d3920925557625a5309ed58eab1367e4e3f
Milad Zarour <Miladzarour@hotmail.com>
2023-02-05 12:36:46 +0100

    transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);

--- Junior Programmer/Create with Code/Prototype 2/Assets/Scripts/PlayerController.cs
+++ Junior Programmer/Create with Code/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float speed;
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +17,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
